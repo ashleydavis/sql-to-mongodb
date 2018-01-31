644ac03f3a1ed7103be082157633e846c21274bb
Ashley Davis <ashley@codecapers.com.au>
2018-01-31 15:16:13 +1000

    Example config file.

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+    sqlConnectionString: "<connection-string-for-your-sql-database-goes-here>", // Insert your connection string here.
+    mongoConnectionString: "mongodb://localhost:27017", // This puts the resulting database in MongoDB running on your local PC.
+    targetDatabaseName: "target-database", // Specify the MongoDB database where the data will end up.
+};
