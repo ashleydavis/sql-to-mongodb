1364ed072eb25b719a76a458492d5b79a57f81b9
Ashley Davis <ashley@codecapers.com.au>
2018-01-31 16:09:30 +1000

    Code to replicate an SQL database to MongoDB.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # sql-to-mongodb
+
 A Node.js script to convert an SQL table to a MongoDB database.
+
+Features
+
+    Automatically detects the primary key for each SQL table and copys it to the MongoDB _id field.
+
+    Automatically copies across types correctly (eg numbers and dates).
+
+## Usage
+
+First install dependencies:
+
+    npm install
+
+Setup:
+
+Edit config.js. Add the connection string for your SQL database. Modify the MongoDB connection string if necessary. Set the name of your target database in MongoDB.
+
+Run:
+
+    node index.js
+
+Wait:
+
+It might take some time to replicate!
+    
---
+++ index.js
@@ -0,0 +1,83 @@
+"use strict";
+
+const mongodb = require('mongodb');
+const sql = require('mssql');
+const E = require('linq');
+
+const config = require("./config.js");
+
+//
+// Replicate an entire SQL table to MongoDB.
+//
+async function replicateTable (tableName, primaryKeyField, targetDb, sqlPool) {
+
+    console.log("Replicating " + tableName + " with primary key " + primaryKeyField);
+
+    const collection = targetDb.collection(tableName);
+    
+    const query = "select * from " + tableName;
+    console.log("Executing query: " + query);
+    const tableResult = await sqlPool.request().query(query);
+
+    console.log("Got " + tableResult.recordset.length + " records from table " + tableName);
+
+    const bulkRecordInsert = E.from(tableResult.recordset)
+        .select(row => {
+            row._id = row[primaryKeyField];
+            return {
+                updateOne: {
+                    filter: { _id: row._id },
+                    update: row,
+                    upsert: true,                    
+                },
+            }            
+        })
+        .toArray();
+
+    await collection.bulkWrite(bulkRecordInsert);
+};
+
+async function main () {
+
+    const mongoClient = await mongodb.MongoClient.connect(config.mongoConnectionString);
+    const targetDb = mongoClient.db(config.targetDatabaseName);
+    
+    const sqlPool = await sql.connect(config.sqlConnectionString);
+    const tablesResult = await sqlPool.request().query(`SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'`);
+
+    const primaryKeysQuery = "SELECT A.TABLE_NAME, A.CONSTRAINT_NAME, B.COLUMN_NAME\n" +
+        "FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS A, INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE B\n" +
+        "WHERE CONSTRAINT_TYPE = 'PRIMARY KEY' AND A.CONSTRAINT_NAME = B.CONSTRAINT_NAME\n" +
+        "ORDER BY A.TABLE_NAME";
+    const primaryKeysResult = await sqlPool.request().query(primaryKeysQuery);
+    const primaryKeyMap = E.from(primaryKeysResult.recordset)
+        .toObject(
+            row => row.TABLE_NAME,
+            row => row.COLUMN_NAME
+        );
+
+    const tableNames = E.from(tablesResult.recordset)
+        .select(row => row.TABLE_NAME)
+        .distinct()
+        .toArray();
+
+    console.log("Replicating SQL tables " + tableNames.join(', '));
+    console.log("It's time for a coffee or three.");
+
+    for (const tableName of tableNames) {
+        await replicateTable(tableName, primaryKeyMap[tableName], targetDb, sqlPool);    
+    }
+
+    await sqlPool.close();
+    await mongoClient.close();
+}
+
+main()
+    .then(() => {
+        console.log('Done');
+    })
+    .catch(err => {
+        console.error("Database replication errored out.");
+        console.error(err);
+    });
+
